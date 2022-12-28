163d34da4118ab70bfa08d033658958ed88112a4
Adrodoc <adrodoc55@googlemail.com>
2022-12-28 23:27:52 +0100

    Pass log-file and log-level from settings to debug adapter (see vanilla-technologies/mcfunction-debugger#59)

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
 	let lastUri: vscode.Uri;
-	context.subscriptions.push(vscode.commands.registerCommand('mcfunction-debugger.askForFunctionPath', () => {
+	context.subscriptions.push(vscode.commands.registerCommand("mcfunction-debugger.askForFunctionPath", () => {
 		let defaultUri = lastUri;
 		if (!defaultUri && vscode.window.activeTextEditor) {
 			defaultUri = vscode.window.activeTextEditor.document.uri;
@@ -18,6 +18,34 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		});
 	}));
+
+	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory("mcfunction", new DebugAdapterDescriptorFactory()));
+}
+
+class DebugAdapterDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
+	createDebugAdapterDescriptor(session: vscode.DebugSession, executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
+		if (!executable) {
+			return executable;
+		}
+		let adapterConfig = vscode.workspace.getConfiguration("mcfunctionDebugger.adapter");
+
+		let args = executable.args;
+		let logFile = adapterConfig.get("log.file");
+		if (logFile) {
+			args.push("--log-file=" + logFile);
+		}
+		let logLevel = adapterConfig.get("log.level");
+		if (logLevel) {
+			args.push("--log-level=" + logLevel);
+		}
+
+		let options = executable.options ?? {};
+		if (!options.cwd) {
+			options.cwd = vscode.workspace.workspaceFolders?.[0]?.uri?.fsPath;
+		}
+
+		return new vscode.DebugAdapterExecutable(executable.command, args, options);
+	}
 }
 
 export function deactivate() { }
